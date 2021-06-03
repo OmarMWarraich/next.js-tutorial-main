c5246d34f966e1c3adfebb88544a152e667f93f9
OmarMWarraich <earthling986@gmail.com>
2021-06-03 18:23:21 +0500

    Render a List of Dummy Meetups

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/9/97/Karlovy_Vary_Sadov%C3%A1_ulice_(1).jpg',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://th.bing.com/th/id/R5adee1d22057903556e9ce5d8a75ce05?rik=NiqX8VYiC0zN0g&pid=ImgRaw',
+        address: 'Some address 10, 12345 Some City',
+        description: 'This is a second meetup!'
+    },
+    {
+        id: 'm3',
+        title: 'A Third Meetup',
+        image: 'https://th.bing.com/th/id/OIP.pF-tTnr0k-9eQpkXLSIG_gHaE8?pid=ImgDet&rs=1',
+        address: 'Some address 15, 12345 Some City',
+        description: 'This is a third meetup!'
+    }
+
+]
+
+
+
+function HomePage() {
+    return(
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    )
+}
+
+export default HomePage;
