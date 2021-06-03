d054b47b8e3f3077a2472e084933371288da2287
OmarMWarraich <earthling986@gmail.com>
2021-06-03 23:02:07 +0500

    Dynamic Pages & getStaticProps & getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,43 @@ function MeetupDetails() {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            { 
+                params: {
+                    meetupId: 'm1',
+            },
+         },
+         { 
+            params: {
+                meetupId: 'm2',
+        },
+     },
+        ],
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: 
+                    'https://upload.wikimedia.org/wikipedia/commons/9/97/Karlovy_Vary_Sadov%C3%A1_ulice_(1).jpg',
+                id: 'meetupId',
+                title: 'First Meetup',
+                address: 'Some Street 5, Some City',
+                description: 'This is a first meetup',    
+            },
+        },
+    }
+}
+
 export default MeetupDetails;
