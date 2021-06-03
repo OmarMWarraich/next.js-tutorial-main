e84e7b4d61a3b9272d5c3cf156c083bc31209f55
OmarMWarraich <earthling986@gmail.com>
2021-06-04 00:31:19 +0500

    Connecting and Querying a mongodb database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,25 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+ 
+async function handler(req, res) {
    if (req.method === 'POST') {
        const data = req.body;
+       
+      const client = await MongoClient.connect('mongodb+srv://omar:Eyecolecta_1@cluster0.apz1s.mongodb.net/meetups?retryWrites=true&w=majority')
+      const db = client.db();
 
+      const meetupsCollection = db.collection('meetups');
+    
+      const result = await meetupsCollection.insertOne({data});
 
-       const { title, image, address, description } = data;
-   }
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({message: 'Meetup inserted!' })
+    }
 }
 
 export default handler;
